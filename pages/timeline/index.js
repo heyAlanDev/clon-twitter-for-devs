8876f5ba8ac07506207c200cfe89964e39e02fc9
alan-cash <contactalanvalencia@gmail.com>
2022-12-04 23:10:25 +0100

    firts class

---
+++ pages/timeline/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export function getStaticProps() {
+  return fetch('http://localhost:3000/api/hello ')
+    .then(res => res.json())
+    .then(response => {
+      const { username } = response
+      console.log(username)
+      return { props: { username } }
+    })
+}
+
+export default function Timeline({ username }) {
+  return (
+    <>
+      <h1>This the timeline of {username}</h1>
+      <Link href='/'>Go home</Link>
+      <style jsx>{`
+        h1 {
+          font-size: 36px;
+          color: red;
+        }
+      `}</style>
+    </>
+  )
+}
